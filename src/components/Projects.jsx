805e83a68db930ead2646011ab776c7839c2e34a
pandeyrohit1410 <pandeyrohit1410@gmail.com>
2023-07-05 14:09:30 +0530

    new update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -44,7 +44,7 @@ const Projects = () => {
 <div class="bg-white mb-5 border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 ">
     <div class="p-5 text-start">
         <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Ecommerce Website</h5>
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">IndiaMArt</h5>
         </a>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">It is an Ecommerce web application</p>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">It is developed using MERN stack.</p>
